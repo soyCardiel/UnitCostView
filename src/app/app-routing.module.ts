fb010bcda9440feed9027c90ff8edd9d1e68845a
SoyCardiel <edcar325@gmail.com>
2020-07-07 21:29:17 -0500

    avances

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./modules/pages/login/login.module').then(m => m.LoginModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
